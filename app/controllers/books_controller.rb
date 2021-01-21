6406d682d4620b51ea41b4635b3c9ba9d49b934a
EC2 Default User <ec2-user@ip-172-31-38-167.ap-northeast-1.compute.internal>
2021-01-21 02:10:24 +0000

    second commit(最終課題解答)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,14 +17,13 @@ class BooksController < ApplicationController
     end
     
     def create
-        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
+        book_params = set_params
         #ストロングパラメータで登録に必要なデータを取り出す。
         @book = Book.new(book_params)
         #データをインスタンス化して@bookに格納する。
         if @book.save
-            flash[:notice] = "家計簿にデータを１件登録しました。"
+            flash[:notice] = "家計簿に#{book_params[:year]}年#{book_params[:month]}月の#{book_params[:category]}を登録しました。"
             redirect_to books_path
-            
         #データを.saveメソッドでセーブしたら一覧画面に移るn
         else
             flash.now[:alert] = "登録に失敗しました。"
@@ -37,10 +36,11 @@ class BooksController < ApplicationController
     end
     
     def update
-        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
+        book_params = set_params
         if @book.update(book_params)
             flash[:notice] = "データを１件更新しました。"
-            redirect_to books_path
+            # redirect_to books_path
+            redirect_to book_path
              #データを.updateメソッドで更新したら一覧画面に移る
         else
             flash.now[:alert] = "更新に失敗しました。"
@@ -57,5 +57,9 @@ class BooksController < ApplicationController
     
     def set_book
         @book = Book.find(params[:id])
+    end
+    
+    def set_params
+         params.require(:book).permit(:year,:month,:inout,:category,:amount)
     end
 end
